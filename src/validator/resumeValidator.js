2eee92d31d2c813089a80b23047ce5dd35d8a1ad
amin havasi <pau.ahq@gmail.com>
2020-10-26 21:32:42 +0330

    validator resume

--- src/validator/resumeValidator.js
+++ src/validator/resumeValidator.js
@@ -11,8 +11,19 @@ const resumeValidator = (body) => {
         email: joi.string().email().required(),
         education: joi.string(),
         achievements: joi.array(),
-        skills: joi.array(),
-        works: joi.array(),
+        skills: joi.array().items(
+            joi.object({
+                name: joi.string().min(3).max(255).required(),
+                rate: joi.number().required(),
+            })
+        ),
+        works: joi.array().items(
+            joi.object({
+                name: joi.string().min(3).max(255).required(),
+                join: joi.string().min(3).max(255).required(),
+                left: joi.string().min(2).max(255).required(),
+            })
+        ),
     });
 
     return schmea.validate(body);
